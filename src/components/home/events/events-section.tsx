771fde1eb62c5ed2349b9c151e879a6d620c10f5
Mehdi Asadli <mehdiasadli@azericard.com>
2026-01-16 14:13:15 +0400

    refactor: events list

--- src/components/home/events/events-section.tsx
+++ src/components/home/events/events-section.tsx
@@ -1,20 +1,14 @@
 import { eventsItems } from './data';
 import { EventBanner } from './event-banner';
-import { EventCard } from './event-card';
 import { EventHeader } from './event-header';
+import { EventItemsList } from './event-items-lits';
 
 export function EventsSection() {
   return (
     <section id='events' className='py-16 md:py-24'>
       <div className='mx-auto max-w-7xl px-4'>
         <EventHeader />
-
-        <div className='mt-12 grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-          {eventsItems.map((event) => (
-            <EventCard key={event.title} event={event} />
-          ))}
-        </div>
-
+        <EventItemsList items={eventsItems} />
         <EventBanner />
       </div>
     </section>

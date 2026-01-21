2227b4ca59cbb0408bd3e70dbf2927a757ca6097
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-01-21 20:55:39 +0400

    chore: remove powered by header and browser source maps on prod

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
 	reactCompiler: true,
 	typedRoutes: true,
+
+	poweredByHeader: false,
+	productionBrowserSourceMaps: false,
+
+	webpack: (config, { dev, isServer }) => {
+		if (!dev && !isServer) {
+			config.devtool = false;
+		}
+
+		return config;
+	},
+
 	images: {
 		remotePatterns: [
 			{

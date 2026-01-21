6cfa825b550ce946f4f024342d1e724195d77fcb
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-01-21 21:00:21 +0400

    fix: remove unsplash from remote patterns

--- next.config.ts
+++ next.config.ts
@@ -22,10 +22,6 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "cdn.sanity.io",
 			},
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-			},
 		],
 	},
 };

5a1f8d41de1e050d386cd7227bca3c2851e572b9
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-01-21 16:31:52 +0400

    fix: scroll with changing the window href (temporary solution)

--- src/components/home/hero/hero-cta.tsx
+++ src/components/home/hero/hero-cta.tsx
@@ -1,19 +1,22 @@
-import { getTranslations } from "next-intl/server";
+"use client";
+
+import { useTranslations } from "next-intl";
 import { LogoIcon } from "@/components/brand/logo";
 import { CTAButton } from "@/components/cta-button";
-import { Link } from "@/i18n/navigation";
 
 interface HeroCTAProps extends React.ComponentProps<typeof CTAButton> {}
 
-export async function HeroCTA({ ...props }: HeroCTAProps) {
-	const t = await getTranslations("home.hero.cta");
+export function HeroCTA({ ...props }: HeroCTAProps) {
+	const t = useTranslations("home.hero.cta");
+
+	const handleScroll = () => {
+		window.location.href = "#memberships";
+	};
 
 	return (
-		<Link href="#memberships">
-			<CTAButton {...props}>
-				<LogoIcon className="size-6" />
-				<span className="mx-2">{t("text")}</span>
-			</CTAButton>
-		</Link>
+		<CTAButton {...props} onClick={handleScroll}>
+			<LogoIcon className="size-6" />
+			<span className="mx-2">{t("text")}</span>
+		</CTAButton>
 	);
 }

e225af379f55fa00a0119fe7fbe43718d24606ed
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-01-21 15:07:25 +0400

    add: field to settings

--- src/sanity/schemaTypes/singletons/settings.ts
+++ src/sanity/schemaTypes/singletons/settings.ts
@@ -1,4 +1,5 @@
 import { CogIcon, EarthGlobeIcon, EnvelopeIcon } from "@sanity/icons";
+import { LayoutGridIcon } from "lucide-react";
 import { defineField, defineType } from "sanity";
 
 export const settings = defineType({
@@ -13,6 +14,11 @@ export const settings = defineType({
 		{ name: "general", title: "General", default: true },
 		{ name: "contact", title: "Contact Information", icon: EnvelopeIcon },
 		{ name: "social", title: "Social Media", icon: EarthGlobeIcon },
+		{
+			name: "pageRendering",
+			title: "Dynamic Page Rendering",
+			icon: LayoutGridIcon,
+		},
 		{ name: "seo", title: "SEO" },
 	],
 	fields: [
@@ -117,6 +123,27 @@ export const settings = defineType({
 			group: "seo",
 			description:
 				"Default SEO settings used when pages don't specify their own",
+		}),
+
+		// page rendering
+		defineField({
+			name: "pageRendering",
+			title: "List of pages to render",
+			type: "array",
+			group: "pageRendering",
+			description:
+				"List of the pages that will be included on the website. For example, if you disable the gallery page, it will both be removed from navbar, and the page will show 404 not found error.",
+			of: [{ type: "string" }],
+			options: {
+				list: [
+					{ title: "Home Page", value: "home" },
+					{ title: "Gallery Page", value: "gallery" },
+					{ title: "Trainers Page", value: "trainers" },
+					{ title: "About Page", value: "about" },
+				],
+				layout: "grid",
+			},
+			initialValue: ["home", "gallery", "trainers", "about"],
 		}),
 	],
 	preview: {
